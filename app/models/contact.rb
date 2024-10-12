6b38a4885153e839537a3e0a939026cf90072eff
cristian@gaviria.org <Cristian Gaviria>
2024-10-11 22:47:50 -0400

    Se agregan las validaciones para los campos iniciales

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,12 @@
 class Contact < ApplicationRecord
+    validates :sex, presence: true
+    validates :birth_date, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :address, presence: true
+    validates :country, presence: true
+    validates :department, presence: true
+    validates :city, presence: true
+    validates :information, presence: true
 end
